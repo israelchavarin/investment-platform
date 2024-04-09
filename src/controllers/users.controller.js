85637afa8505043cf5242dc26747449b3d7b84c5
israelchavarin <israelchavarin2d@gmail.com>
2024-04-08 20:47:52 -0600

    Add functionality to update user data

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs';
 import User from '../database/models/user.js';
 import UserAccess from '../database/models/userAccess.js';
 import UserBalance from '../database/models/userBalance.js';
+import { sequelize } from '../config/database.config.js';
 
 /** List users function
  * Input: none
@@ -34,18 +36,72 @@ export const listUsers = async (req, res) => {
   }
 };
 
-/** Update user data function
- * Input: xxxxxx
- * Data output: [updatedUser]
+/** Update user function
+ * Input: givenName, familyName, email, password
+ * Data output: none
  */
-export const updateUserData = async (req, res) => {
+export const updateUser = async (req, res) => {
+  const t = await sequelize.transaction();
+
   try {
-    const { givenName } = req.body;
+    const { id } = req.user;
+    const {
+      givenName, familyName, email, password,
+    } = req.body;
+
+    const loggedUser = await User.findOne({
+      where: { user_id: id, is_hidden: false },
+      transaction: t,
+    });
+    const loggedUserAccess = await UserAccess.findOne({
+      where: { user_id: id, is_hidden: false },
+      transaction: t,
+    });
+
+    if (!loggedUser || !loggedUserAccess) {
+      await t.rollback();
+      return res.status(400).json({
+        status: 400,
+        error: 'User could not be identified',
+      });
+    }
+
+    // New email validation
+    const existingAccess = await UserAccess.findOne({
+      where: { email, is_hidden: false },
+      transaction: t,
+    });
+
+    if (existingAccess) {
+      await t.rollback();
+      return res.status(400).json({
+        status: 400,
+        error: 'Email provided already in use',
+      });
+    }
+
+    // Update of user
+    loggedUser.given_name = givenName;
+    loggedUser.family_name = familyName;
+
+    await loggedUser.save({ transaction: t });
+
+    // Update of their accesses
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    loggedUserAccess.email = email;
+    loggedUserAccess.password = hashedPassword;
+
+    await loggedUserAccess.save({ transaction: t });
+
+    await t.commit();
+
     return res.status(200).json({
       status: 200,
-      data: { givenName },
+      message: 'Information updated successfully',
     });
   } catch (error) {
+    await t.rollback();
     return res.status(500).json({
       status: 500,
       error: error.message,
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
-import { listUsers } from '../controllers/users.controller.js';
+import { listUsers, updateUser } from '../controllers/users.controller.js';
+import validateSchema from '../middlewares/validateSchema.middleware.js';
+import { updateUserDataSchema } from './validations/user.schema.js';
+import authenticate from '../middlewares/authenticate.middleware.js';
 
 const router = Router();
 
@@ -7,5 +10,10 @@ const router = Router();
  *  /api/users
  */
 router.get('/', listUsers);
+
+/** PATCH
+ *  /api/users/update/:id
+ */
+router.patch('/update/:id', authenticate, validateSchema(updateUserDataSchema), updateUser);
 
 export default router;
